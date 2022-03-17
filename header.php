40f363eae9c54cb1a3d04ad75639907a4733d6f2
JMPBruhn <91875955+JMPBruhn@users.noreply.github.com>
2022-03-17 14:46:04 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+<html lang="en">
+	
+<head>
+
+	<title> SixS </title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
+	
+</head>
+
+<body>
+
+	<header>
+		
+		<div style="text-align: center;">
+			<a href="index.html"><img src="picture.png" width="300" height="400" alt="Picture of logo" ></a></div>
+	    	<div style="text-align: center; padding-bottom: 10px; font-size: 28px;">Your Data our Drive.</div>
+	    	<div style="text-align: center; padding-top: 30px; padding-bottom: 50px; font-size: 18px;">SixS offers a tailored approaches to map your operations waiting time, processing, and movement in numbers.</div>	
+	    
+	</header>
+	
+
+
+<div class="navbar">
+  <a href="Stories.html" title="Stories">Stories</a> 
+  <a href="Skill.html">Skill</a>
+  <a href="Store.html">Store</a>
+  <a href="Software.html">Software</a>
+  <a href="Shine.html">Shine</a>
+  <a href="Standardize.html">Standardize</a>
+  <a href="login.html">Login</a>
+
+
+</div>
